d2ea489d65e4255f4cd7f0dafd86756dbb7ade15
e <207416530@qq.com>
2019-04-15 15:31:53 +0800

    配置中心修改

--- aigou_parent/aigou_config_server/src/main/java/com/lh/aigou_config_server/AigouConfigServerApplication.java
+++ aigou_parent/aigou_config_server/src/main/java/com/lh/aigou_config_server/AigouConfigServerApplication.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient //加入注册中心
 @EnableConfigServer //启用配置服务端
 public class AigouConfigServerApplication {
 

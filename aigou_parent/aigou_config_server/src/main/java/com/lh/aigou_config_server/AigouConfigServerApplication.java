1d92cb25986d542e059dddcdc795586aefb6cb18
e <207416530@qq.com>
2019-04-15 10:58:13 +0800

    配置中心修改

--- aigou_parent/aigou_config_server/src/main/java/com/lh/aigou_config_server/AigouConfigServerApplication.java
+++ aigou_parent/aigou_config_server/src/main/java/com/lh/aigou_config_server/AigouConfigServerApplication.java
@@ -2,8 +2,12 @@ package com.lh.aigou_config_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
 public class AigouConfigServerApplication {
 
     public static void main(String[] args) {
